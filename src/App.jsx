698d70ef3674203b66546171f63a034407b71c77
Marcelo Berrio <betamagicc@icloud.com>
2026-08-18 19:13:59 -0500

    Remove unused booking and admin code

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,19 @@
 import { useState } from "react";
 import Home from "./pages/Home";
-import Booking from "./pages/Booking";
 import ExtraServices from "./pages/ExtraServices";
-
-
 import Contact from "./pages/Contact";
 
 function App() {
   const [page, setPage] = useState("home");
-  const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(
-    localStorage.getItem("isAdminLoggedIn") === "true"
-  );
-
-  function handleLogout() {
-    localStorage.removeItem("isAdminLoggedIn");    
-    window.location.href = "/";
-  }
 
   return (
     <>
       {page === "home" && (
         <Home 
-          goBooking={() => setPage("booking")}
+          goBooking={() => setPage("contact")}
           goExtraServices={() => setPage("extraServices")}
           goContact={() => setPage("contact")}
-          goAdmin={() => setPage(isAdminLoggedIn ? "admin" : "adminLogin")}
           goHome={() => setPage("home")}
-          isAdminLoggedIn={isAdminLoggedIn}
-          handleLogout={handleLogout}
-        />
-      )}
-
-      {page === "booking" && (
-        <Booking 
-          goHome={() => setPage("home")}
-          goExtraServices={() => setPage("extraServices")}
-          goAdmin={() => setPage(isAdminLoggedIn ? "admin" : "adminLogin")}
-          isAdminLoggedIn={isAdminLoggedIn}
-          handleLogout={handleLogout}
         />
       )}
 
@@ -46,12 +22,8 @@ function App() {
           goHome={() => setPage("home")} 
           goExtraServices={() => setPage("extraServices")}
           goContact={() => setPage("contact")}
-          goAdmin={() => setPage(isAdminLoggedIn ? "admin" : "adminLogin")}
-          isAdminLoggedIn={isAdminLoggedIn}
-          handleLogout={handleLogout}
         />
       )}
-
 
       {page === "contact" && (
         <Contact goHome={() => setPage("home")} />
